8b904a67fd1d6479786f21ec94261d8251cabd5a
Roni <roni.jimmylee@gmail.com>
2021-07-27 22:17:35 -0300

    persistencia json ler e gravar

--- src/main/java/util/LeitorJson.java
+++ src/main/java/util/LeitorJson.java
@@ -2,6 +2,8 @@ package util;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,8 +16,26 @@ public class LeitorJson {
 	static Gson gson = new Gson();
 	static Reader reader;
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 
+		lerArquivoJson();
+		gravaArquivoJson();
+	}
+
+	
+	private static void gravaArquivoJson() throws IOException {
+
+		mapJson.put("url", "htpps://qaacademy.com,br");
+		mapJson.put("curso", "teste automatizado");
+		mapJson.put("endereço", "Paulista, 509");
+		
+		FileWriter writer = new FileWriter("./src/main/resources/massaTeste_writer.json");
+		gson.toJson(mapJson, writer);
+				writer.flush();
+		
+	}
+	
+	private static void lerArquivoJson() throws FileNotFoundException {
 		reader = new FileReader("./src/main/resources/massaTeste.json");
 		mapJson = gson.fromJson(reader, Map.class);
 		
@@ -23,5 +43,4 @@ public class LeitorJson {
 		System.out.println(mapJson.get("produto"));
 		System.out.println(mapJson.get("valor"));
 	}
-
 }
